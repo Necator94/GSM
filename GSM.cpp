89e55c335a2043a063cf27bdd3fc4619d45d7c59
Ivan Matveev <necator94@gmail.com>
2015-12-15 13:23:37 +0100

    comenst added, ET & LO added

--- GSM.cpp
+++ GSM.cpp
@@ -10,105 +10,95 @@ int main()
 {
 
 	//Every AT command starts with "AT", sync baud rate
-
-	string msgTxtMode="AT+CMGF=1\n";                        		// Set SMS message command as text mode
-	string sms="AT+CMGS=\"+491748075141\"\r";						// Sending SMS to desired number
-	string readMsg="AT+CMGR=1\n";        							// Command for reading message from location 1 from inbox
-	string deleteMessageByPosition="AT+CMGD=1\n";         			// Erasing all wrote messages from inbox
-
-	string ctrlD="\x1A";											// Symbols which are equivalent to pressing Ctrl + D buttons
-	//char* checkMessageStatus="AT+CMGL=\"ALL\"";        			// Check status of received SMS
+	string msgTxtMode="AT+CMGF=1\n";             	// Set SMS message command as text mode
+	string sms="AT+CMGS=\"+491748075141\"\r";		// Sending SMS to desired number
+	string readMsg="AT+CMGR=1\n";        			// Command for reading message from location 1 from inbox
+	string deleteMessageByPosition="AT+CMGD=1\n";   // Erasing all wrote messages from inbox
+	string ctrlD="\x1A";							// Symbols which are equivalent to pressing Ctrl + D buttons
+	//char* checkMessageStatus="AT+CMGL=\"ALL\"";   // Check status of received SMS
 	string k1 = "\"";
-	string bEnter = "\r";											// Symbols which are equivalent to pressing Enter button
+	string bEnter = "\r";							// Symbols which are equivalent to pressing Enter button
 
-
-
-
-
-
-
-
-
-
-
-	ADC_UART_enabling();															// Initialization of ADC and UART
-	send(deleteMessageByPosition);													// SMS deleting
-
-	double T = T_ADC_reading();
-
+	ADC_UART_enabling();							// Initialization of ADC and UART
+	send(deleteMessageByPosition);					// SMS deleting
 
 
 	while (true){
 
 		// Reading of SMS
-		send(msgTxtMode);															// Set text mode on GSM M95 click
-		string answ = send(readMsg);												// Reading SMS to string answ
+		send(msgTxtMode);							// Set text mode on GSM M95 click
+		string answ = send(readMsg);				// Reading SMS to string answ
 
 		//	Splitting of SMS
-		string w1, w2, w3;
-		fun(&answ, &w1, &w2, &w3);													// Split string on variables via fun function
+		string w1, w2, w3;							//Initialization of variables
+		fun(&answ, &w1, &w2, &w3);					// Split string on variables via fun function
 		cout << "msg content   "<< answ << w1 << w2 << endl;
 
 		// Initialization(log in) of user
-		if (answ=="L"){																// If first word of SMS is "L" ...
-			string userName = w1;													// Write second word(name of user) to string userName
-			string telNumber = "AT+CMGS=" + k1 + w2 + k1 + bEnter;					// Write third word(telephone number) to string telNumber
-			send(telNumber);														// Enter of telephone number to GSM module
-			string confirm = "Login accepted!\x1A";
+		if (answ=="L"){												// If first word of SMS is "L" ...
+			string userName = w1;									// Write second word(name of user) to string userName
+			string telNumber = "AT+CMGS=" + k1 + w2 + k1 + bEnter;	// Write third word(telephone number) to string telNumber
+			send(telNumber);										// Enter of telephone number to GSM module
+			string confirm = "Login accepted!\x1A";					// Text of string that will send
 			cout << "Login accepted" << endl;
-			send(confirm);															// Send string which contains SMS with text of login confirmation
+			send(confirm);											// Send string which contains SMS with text of login confirmation
 			send(deleteMessageByPosition);
 
 			// Working cycle of program which includes modes
 			bool k1 = true;
 			while (k1 == true){
 
-				// Reading and splitting of SMS
+				// Reading and splitting of SMS (described above)
 				send(msgTxtMode);
 				string answ = send(readMsg);
 				string w1, w2, w3;
 				fun(&answ, &w1, &w2, &w3);
 
-					// Status mode
-					if ((answ == userName) && (w1 == "status")){
-						double T = T_ADC_reading();									// Reading of ADC value via function
+					// Check status
+					if ((answ == userName) && (w1 == "status")){	// Chech SMS for "Username" and "status"
+						double T = T_ADC_reading();					// Reading of ADC value via function
 						cout << T << endl;
 						cout << telNumber << endl;
 
 						// Temperature conversion from double to string
-						stringstream strRt;
+						stringstream strRt;			 // Conversion is completed via stringstream class
 						string strT;
 						strRt << T;
-						strRt >> strT;
+						strRt >> strT;				// Variable strT contains temperature as a string type
 
 						// Sending of SMS with current temperature
-						string msg = "Current temperature: " + strT + ctrlD;
-						send(telNumber);
-						sleep(1);
-						send(msg);
+						string msg = "Current temperature: " + strT + ctrlD;  // Creation of string for sending
+						send(telNumber);			// Entering of tel. number to GMS module
+						sleep(1);					// Pause
+						send(msg);					// Sending of string that was recently created
 						sleep(1);
 						cout << "Temperature sent by status1" << endl;
-						send(deleteMessageByPosition);
+						send(deleteMessageByPosition);	// Clear message buffer
 						sleep(1);
 					}
-
-					// Tracking mode
-					if ((answ == userName) && (w1 == "T")){
+					// Log out from system
+					if ((answ == userName) && (w1 == "LO")){
+						send(deleteMessageByPosition);
+						k1 = false;
+					}
+					// Enabling of tracking mode
+					if ((answ == userName) && (w1 == "T")){		// Chech SMS for "Username" and "T"
 						bool k2 = true;
 
-						// Reading and splitting of SMS
+						// Reading and splitting of SMS (described above)
 						send(msgTxtMode);
 						answ = send(readMsg);
 						fun(&answ, &w1, &w2, &w3);
-						//Conversion of strings with temerature range to double type
-						double tMin = atof(w2.c_str());
-						double tMax = atof(w3.c_str());
+						//Conversion of strings with temperature range to double type
+						double tMin = atof(w2.c_str());		// Convert third word to tMin as double
+						double tMax = atof(w3.c_str());		// Convert fourth word to tMax as double
 						cout << tMin << " - tMin" << endl;
 						cout << tMax << " - tMax" << endl;
-						send(deleteMessageByPosition);
+						send(deleteMessageByPosition);    // Clear message buffer
 
-						while (k2 == true){											// Until SMS with warning will be send
+						while (k2 == true){				  // Until k2 != false
 
+							// Reading and splitting of SMS (described above)
 							send(msgTxtMode);
 							answ = send(readMsg);
 							fun(&answ, &w1, &w2, &w3);
@@ -120,14 +110,16 @@ int main()
 							sleep(1);
 							// Check on temperature conformity condition
 							if ((T < tMin) || (T > tMax)){
+								// Temperature conversion from double to string (described above)
 								stringstream strRt1;
 								string strT1;
 								strRt1 << T;
 								strRt1 >> strT1;
-								string msg = "Warning! Current temperature:  " + strT1 + ctrlD;
+
+								string msg = "Warning! Current temperature:  " + strT1 + ctrlD; // String creation
 								send(telNumber);
 								sleep(1);
-								send(msg);
+								send(msg);			// Send warning message to user
 								sleep(1);
 								cout << "Temperature sent by T command" << endl;
 								send(deleteMessageByPosition);
@@ -152,13 +144,16 @@ int main()
 								send(deleteMessageByPosition);
 								sleep(1);
 							}
-
+							//End of cycle without SMS deleting in case of new boundaries
 							if ((answ == userName) && (w1 == "T")){
 							k2 = false;
-
-
 							}
 
+							//End of cycle
+							if ((answ == userName) && (w1 == "ET")){
+							send(deleteMessageByPosition);
+							k2 = false;
+							}
 						}
 					}
 			}
--- functions.cpp
+++ functions.cpp
@@ -4,58 +4,61 @@
 #include "functions.h"
 
 
-#define DEVICE_PORT "/dev/ttyO4"												// Path to serial port
+#define DEVICE_PORT "/dev/ttyO4" 	// Path to serial port on BeagleBone Black
 
 //Sending commands to device
 string send(string command)
 {
 	serialib LS;
-		const char * comm = command.c_str();
+		const char * comm = command.c_str();//conversion of input argument to char * type which is nessecary for seriallib
 		int Ret;
-		char Buffer[128];
-		Ret = LS.Open(DEVICE_PORT, 9600);                                      	// Open serial link at 9600 bauds
-		Ret = LS.WriteString(comm);                                            	// Send the command on the serial port
-		string chek_reading_a(comm);
-		string chek_reading_b = "AT+CMGR=1\n";
-		if (chek_reading_a == chek_reading_b){
-			Ret = LS.ReadString(Buffer,'\n',128,5000);   						// Read a maximum of 128 characters with a timeout of 5 seconds
+		char Buffer[128];					// Define size of buffer (amount of symbols in SMS)
+		Ret = LS.Open(DEVICE_PORT, 9600);   // Open serial link at 9600 bauds
+		Ret = LS.WriteString(comm);         // Send the command on the serial port
+		string chek_reading_a(comm);		// Conversion read value to string type
+
+		string chek_reading_b = "AT+CMGR=1\n";  		// Reading SMS string for next condition
+		if (chek_reading_a == chek_reading_b){  		// Check for command on presence of reading sms command
+			// Read a maximum of 128 characters with a timeout of 5 seconds
+			// The final character of the string must be a line feed ('\n')
+			Ret = LS.ReadString(Buffer,'\n',128,5000);	// Read first string from GSM module answer (send command)
+			Ret = LS.ReadString(Buffer,'\n',128,5000);	// Read second string from GSM module answer (GSM answer)
+			Ret = LS.ReadString(Buffer,'\n',128,5000);	// Read third string from GSM module answer (SMS text)
+			string str(Buffer);							// Write string to buffer
+			return str;									// Return buffer as argument
+		} else {  // This condition used for debugging
 			Ret = LS.ReadString(Buffer,'\n',128,5000);
-			Ret = LS.ReadString(Buffer,'\n',128,5000);							// The final character of the string must be a line feed ('\n')
-			string str(Buffer);
-			return str;
-		} else {
-			Ret = LS.ReadString(Buffer,'\n',128,5000);
-			Ret = LS.ReadString(Buffer,'\n',128,5000);							// Read a second string from the serial device
+			Ret = LS.ReadString(Buffer,'\n',128,5000);	// Read an answer of GSM module
 			string str1(Buffer);
 			return str1;
 		}
-   LS.Close();																	// Close the connection with the device
+   LS.Close();											// Close the connection with the device
 return string();
 }
 
 //Splitting of strings on words
 string fun(string *answ, string* w1, string* w2, string* w3)
 {
-	 	string buf; 															// Have a buffer string
-	    stringstream ss(*answ); 												// Insert the string into a stream
-	    vector <string> tokens; 												// Create vector to hold our words
+	 	string buf; 					// Have a buffer string
+	    stringstream ss(*answ); 		// Insert the string into a stream
+	    vector <string> tokens; 		// Create vector to hold our words
 	    //while (ss >> buf)
 	    ss >> buf;
 	    tokens.push_back(buf);
-	    *answ = buf;
+	    *answ = buf;					// Write first word to *answ
 	    ss >> buf;
 	    tokens.push_back(buf);
-	    *w1 = buf;
+	    *w1 = buf;						// Write second word to *w1
 	    ss >> buf;
 	    tokens.push_back(buf);
-	    *w2 = buf;
+	    *w2 = buf;						// Write third word to *w2
 	    ss >> buf;
 	    tokens.push_back(buf);
-	    *w3 = buf;
+	    *w3 = buf;						// Write fourth word to *w3
         return string();
 }
 
-//Check on existence of file
+//Check on existence of file (uses for checking ADC and UART)
 bool FileExists(const char *fname)
 		{
 		return ifstream(fname) != NULL;
@@ -64,19 +67,19 @@ bool FileExists(const char *fname)
 //ADC and serial port enabling
  void ADC_UART_enabling()
 {
-	 const char *fnameADC = "/sys/devices/ocp.3/helper.15";
-	 const char *fnameUART = "/dev/ttyO4";
-	 if (FileExists(fnameADC) == 0){
-		 string adcarg = "cape-bone-iio";
-		 ofstream adc_on ("/sys/devices/bone_capemgr.9/slots");
-		 adc_on << adcarg;
+	 const char *fnameADC = "/sys/devices/ocp.3/helper.15";    	// Path to ADC directory for existing check
+	 const char *fnameUART = "/dev/ttyO4";						// Path to UART directory for existing check
+	 if (FileExists(fnameADC) == 0){							// Enable ADC in case if it was not enabled earlier
+		 string adcarg = "cape-bone-iio";						// Enabling command
+		 ofstream adc_on ("/sys/devices/bone_capemgr.9/slots"); // Path to configuration directory for ADC
+		 adc_on << adcarg;										// Write command to file
 		 adc_on.close();
 		 cout << "ADC_enabled" << endl;
 	}
-	 if (FileExists(fnameUART) == 0){
-		 string UARTarg = "BB-UART4";
-		 ofstream UART_on ("/sys/devices/bone_capemgr.9/slots");
-		 UART_on << UARTarg;
+	 if (FileExists(fnameUART) == 0){							// Enable UART in case if it was not enabled earlier
+		 string UARTarg = "BB-UART4";							// Enabling command
+		 ofstream UART_on ("/sys/devices/bone_capemgr.9/slots");// Path to configuration directory for ADC
+		 UART_on << UARTarg;									// Write command to file
 		 UART_on.close();
 		 cout << "UART_enabled" << endl;
 	 }
@@ -85,21 +88,15 @@ bool FileExists(const char *fname)
  //Reading and processing of ADC value to temperature
 double T_ADC_reading()
 {
-	// ifstream adc_val1 ("/sys/devices/ocp.3/helper.15/AIN0");
-	 ifstream adc_val ("/sys/bus/iio/devices/iio\:device0/in_voltage0_raw");
+	// ifstream adc_val1 ("/sys/devices/ocp.3/helper.15/AIN0");				// Path to file which contains value of ADC(0-1800)
+	 ifstream adc_val ("/sys/bus/iio/devices/iio\:device0/in_voltage0_raw"); // Path to file which contains value of ADC(0-4096)
 	 double adc_value;
-	 adc_val >> adc_value;
-
-
-
-	 adc_value = 1.8 / 4095 * adc_value;
+	 adc_val >> adc_value;								// Write ADC value to variable
+	 adc_value = 1.8 / 4095 * adc_value;				// Calculation
 	// cout << adc_value << " - 4096" << endl;
-	 double Rt = (800 * adc_value) / (1.8 - adc_value);
-	 double T = (Rt - 1000) * 0.25;
+	 double Rt = (800 * adc_value) / (1.8 - adc_value); // Calculation of resulted resistance
+	 double T = (Rt - 1000) * 0.25;						// Calculation of resulted temperature
 	// cout << T << " - 4096" << endl;
-
-
-
-	 return T;
+	 return T;											// Return resulted temperature as argument
 }
 
